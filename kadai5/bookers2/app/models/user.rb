2cebf910d7bd2c05685efb760c766dad31dcf4e5
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-04-06 11:51:57 +0900

    [finish]kadai5

---
+++ kadai5/bookers2/app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,:validatable
+
+  has_many :books, dependent: :destroy
+
+  has_many :post_comments, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
+
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+    def following?(other_user)
+      following_relationships.find_by(following_id: other_user.id)
+    end
+
+    def follow!(other_user)
+      following_relationships.create!(following_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+      following_relationships.find_by(following_id: other_user.id).destroy
+    end
+
+  def self.search(search,word)
+    if search == "perfect_match"
+      @user = User.where(name: "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+      elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+      elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+      else
+      @user = User.all
+     end
+  end
+
+  attachment :profile_image
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, length: {maximum: 20, minimum: 2}
+  validates :name, presence: true
+  validates :introduction, length: {maximum: 50}
+end
